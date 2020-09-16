5fed823248fed935d96483890d9771c3c0d71e74
xlshi <942309989@qq.com>
2020-09-16 15:43:03 +0800

    add Class Node

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,10 @@
-import java.util.LinkedList;
-
 /**
  * @author : Code Dragon
  * create at:  2020/7/20  8:56
  */
 class Solution {
-    public int[] reversePrint(ListNode head) {
-        LinkedList<Integer> result = new LinkedList<>();
-        while (head.next != null) {
-            result.addFirst(head.val);
-            head = head.next;
-        }
-        int[] res = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            res[i] = result.get(i);
-        }
-        return res;
-    }
-
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+    public TreeNode invertTree(TreeNode root) {
 
     }
 }
+
