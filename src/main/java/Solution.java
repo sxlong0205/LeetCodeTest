fc6583190fde6cd4017bde7800eed58086372ec2
xlshi <942309989@qq.com>
2020-11-23 16:26:51 +0800

    更新listnode

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,33 +1,11 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author : Code Dragon
  * create at:  2020/7/20  8:56
  */
 public class Solution {
-    public int[] levelOrder(TreeNode root) {
-        if (root == null) return new int[]{};
-        List<Integer> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
 
-        while (!queue.isEmpty()) {
-            TreeNode temp = queue.poll();
-            result.add(temp.val);
-            if (temp.left != null)
-                queue.add(temp.left);
-            if (temp.right != null)
-                queue.add(temp.right);
-
-        }
-
-        int[] temp = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            temp[i] = result.get(i);
-        }
-        return temp;
     }
 }
