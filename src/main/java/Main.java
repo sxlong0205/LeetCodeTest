e85dd97f60065bebf334b4b53a1ae37f378c6354
sxlong0205 <942309989@qq.com>
2021-09-14 08:52:35 +0800

    更新

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,28 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int k = 0; k < t; k++) {
-            int n = in.nextInt();
-            int m = in.nextInt();
-
-            int totalSum = (1 + (m * n)) * (m * n) / 2;
-            int rowIndex = -1, columIndex = -1;
-            int rowDivd = 0, columDivd = 0;
-
-            int halfSum = 0;
-            if (totalSum % 2 == 0) {
-                halfSum = totalSum / 2;
-            } else {
-                halfSum = totalSum / 2 + 1;
-            }
-
-            // 横向，利用前缀和思想
-            int[] rowPreSum = new int[n + 1];
-            for (int i = 1; i <= n; i++) {
-                rowPreSum[i] = rowPreSum[i - 1] + ((1 + m) * m / 2 + (i - 1) * m * m);
-            }
-            for (int i = 1; i <= n; i++) {
-                if (rowPreSum[i] > halfSum) {
-                    rowIndex = i;
-                    rowDivd = totalSum - rowPreSum[i - 1] * 2;
-                    break;
-                }
-            }
-
-            // 纵向，利用前缀和思想
-            int[] columPreSum = new int[m + 1];
-            for (int i = 1; i <= m; i++) {
-                columPreSum[i] = columPreSum[i - 1] + ((1 + 1 + m * (n - 1)) * n / 2 + n * (i - 1));
-            }
-            for (int i = 1; i <= m; i++) {
-                if (columPreSum[i] > halfSum) {
-                    columIndex = i;
-                    columDivd = totalSum - columPreSum[i - 1] * 2;
-                    break;
-                }
-            }
-
-            if (columDivd >= rowDivd) {
-                System.out.println("H " + rowIndex);
-            } else {
-                System.out.println("V " + columIndex);
-            }
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root.val == subRoot.val) {
+            return dfs(root, subRoot);
+        } else {
+            boolean left = isSubtree(root.left, subRoot);
+            boolean right = isSubtree(root.right, subRoot);
+            return left || right;
         }
+    }
+
+    private static boolean dfs(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+
+        boolean left = dfs(root.left, subRoot.left);
+        boolean right = dfs(root.right, subRoot.right);
+        return root.val == subRoot.val && left && right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = ConstructTree.constructTree(new Integer[]{1, 2, 3, 4, 5});
+        TreeNode subRoot = ConstructTree.constructTree(new Integer[]{1, 2, 3});
+        boolean result = isSubtree(root, subRoot);
+        System.out.println(result);
     }
 }
